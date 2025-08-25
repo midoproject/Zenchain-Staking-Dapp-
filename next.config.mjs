4eff2e96d44010e936c1a782f53029bf1d31a5a4
Mido_0711 <ujangkurniasandi711@gmail.com>
2025-08-25 16:55:49 +0700

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true }
+  experimental: {
+    typedRoutes: true,
+    tsconfigPaths: true   // <— tambahkan ini
+  }
 };
 export default nextConfig;
